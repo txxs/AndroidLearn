22dd8f8e81bde5feda9f13efd6caf530db06faec
txxs <sdlyjhm01@sina.com>
2017-06-26 00:51:21 +0800

    fragemtn

--- 2017-06-25/src/main/java/com/realize/txxs/androidlearn/FraActivity.java
+++ 2017-06-25/src/main/java/com/realize/txxs/androidlearn/FraActivity.java
@@ -3,6 +3,9 @@ package com.realize.txxs.androidlearn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by txxs on 2017/6/25.
@@ -10,10 +13,21 @@ import android.os.Bundle;
 
 public class FraActivity extends Activity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //在这个Activity中加载定义的Fragment中的内容
         setContentView(R.layout.fractivity);
+        Button button = (Button)findViewById(R.id.buttonf);
+        button.setText("改变");
+        textView = (TextView)findViewById(R.id.textf);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("framen中的值发生改变了");
+            }
+        });
     }
 }
